60b49302ce3777f88cad4e2a82fab30ee50b421b
Andrew Eames <ajerxs@gmail.com>
2021-02-09 18:31:52 +0000

    Additional scraping ideas in Best class

--- lib/best_selling_games/best.rb
+++ lib/best_selling_games/best.rb
@@ -1,4 +1,5 @@
 class BestSellingGames::Best
+  attr_accessor :name
 
   def self.best_list
     best_games = []
@@ -10,8 +11,9 @@ class BestSellingGames::Best
 
   def self.best_scrape
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_best-selling_video_games"))
-    # things = doc.css("tr td i").text; this needs some fixing to play with
-    binding.pry
+    things = doc.css("tr td i").each.with_index(1) do |a, i|
+      puts "#{i}. #{a.text}"
+    end
   end
 
 end
