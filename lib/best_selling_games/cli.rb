382d706bcd502cb8860e750ae76bd9fd8307253b
Andrew Eames <ajerxs@gmail.com>
2021-02-09 00:29:49 +0000

    select_list

--- lib/best_selling_games/cli.rb
+++ lib/best_selling_games/cli.rb
@@ -9,6 +9,17 @@ class BestSellingGames::CLI
     puts "Enter the number of the list would you like to display:"
     puts "1. Best-Selling Video Games"
     puts "2. Most-Played Video Games by Player Count"
+    input = gets.strip.downcase
+    if input == "1"
+      puts "Here's a list of the best-selling video games of all time:"
+    elsif input == "2"
+      puts "Here's a list of the most-played video games of all time:"
+    elsif input == "exit"
+      puts "See you later!"
+    else
+      puts "Sorry, I didn't quite get that!"
+      select_list
+    end
   end
 
 end
