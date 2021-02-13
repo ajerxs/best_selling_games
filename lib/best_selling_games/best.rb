bf27c334996b17f4e5be89fdd92caafed4184a5d
Andrew Eames <ajerxs@gmail.com>
2021-02-13 21:02:24 +0000

    Nested array, still need to remove \n from some strings

--- lib/best_selling_games/best.rb
+++ lib/best_selling_games/best.rb
@@ -11,9 +11,16 @@ class BestSellingGames::Best
 
   def self.best_scrape
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_best-selling_video_games"))
+  end
+
+  def self.best_elements
+    self.best_scrape.css("table.wikitable tbody tr")
+  end
+
+  def self.make_best
     nu_array = []
-    things = doc.css("table.wikitable tbody tr").each do |a|
-      b = a.text
+    self.best_elements.each do |a|
+      b = a.text.split("\n\n")
       nu_array << b
     end
     nu_array.shift
