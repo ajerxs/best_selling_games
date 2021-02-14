a50ea84c79ca91b076cf774bcfff7767e6eb1918
Andrew Eames <ajerxs@gmail.com>
2021-02-14 00:01:26 +0000

    Working on making instances of best games

--- lib/best_selling_games/best.rb
+++ lib/best_selling_games/best.rb
@@ -1,13 +1,19 @@
 class BestSellingGames::Best
-  attr_accessor :name
+  attr_accessor :name, :copies_sold, :platform
 
-  def self.best_list
-    best_games = []
+  @@all = []
 
-    best_games << best_scrape
-
-    best_games
+  def initialize
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  # def self.best_list
+    # will need this method eventually
+  #end
 
   def self.best_scrape
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_best-selling_video_games"))
@@ -17,15 +23,24 @@ class BestSellingGames::Best
     self.best_scrape.css("table.wikitable tbody tr")
   end
 
-  def self.make_best
-    nu_array = []
+  def self.best_array
+    @nu_array = []
     self.best_elements.each do |a|
       b = a.text.split("\n\n")
-      nu_array << b
+      @nu_array << b
     end
-    nu_array.shift
-    nu_array
-    # things.gsub(/\n/, ',')
+    @nu_array.shift
+    @nu_array
+  end
+
+  def self.make_best
+    self.best_array.each do |a|
+      best = BestSellingGames::Best.new
+      best.name = a[1]
+      best.copies_sold = a[2]
+      best.platform = a[3]
+    end
+    binding.pry
   end
 
 end
