5dd4b333e49340150f85cd7b829325fb1c6ccf8f
Andrew Eames <ajerxs@gmail.com>
2021-02-15 21:25:00 +0000

    Fix best_array, very close to what is needed

--- lib/best_selling_games/best.rb
+++ lib/best_selling_games/best.rb
@@ -1,5 +1,5 @@
 class BestSellingGames::Best
-  attr_accessor :name, :copies_sold, :platform
+  attr_accessor :rank, :name, :copies_sold, :platform
 
   @@all = []
 
@@ -25,7 +25,7 @@ class BestSellingGames::Best
     self.best_scrape.css("table.wikitable tbody tr")
   end
 
-  def self.best_array
+  def self.first_array
     @nu_array = []
     self.best_elements.each do |a|
       b = a.text.split("\n\n")
@@ -35,14 +35,26 @@ class BestSellingGames::Best
     @nu_array
   end
 
+  def self.best_array
+    @bst_array = []
+    self.first_array.each do |a|
+      a.each do |b|
+        c = b.gsub("\n", "")
+        @bst_array << c
+      end
+    end
+    @bst_array
+  end
+
   def self.make_best
     self.best_array.each do |a|
       best = BestSellingGames::Best.new
+      best.rank = a[0]
       best.name = a[1]
       best.copies_sold = a[2]
       best.platform = a[3]
     end
-    @@all[49]
+    @@all[0]
   end
 
 end
